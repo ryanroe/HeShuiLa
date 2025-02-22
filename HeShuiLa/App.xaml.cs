40e60b16cb02b9349933e9e39634be33e10c8dbe
Ryan <smbserv@qq.com>
2025-02-22 09:11:07 +0800

    ✨ Implement application mutex to prevent multiple instances from running

--- HeShuiLa/App.xaml.cs
+++ HeShuiLa/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,6 +14,38 @@ namespace HeShuiLa
     /// </summary>
     public partial class App : Application
     {
+        private static Mutex _mutex = null;
         public MainVM MainVM { get; } = new MainVM();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string appName = "HeShuiLa";
+            bool createdNew;
+
+            _mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show("应用程序已经在运行中", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
+                Current.Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (_mutex != null)
+            {
+                try
+                {
+                    _mutex.ReleaseMutex();
+                    _mutex.Dispose();
+                }
+                catch (ApplicationException) { }
+            }
+            base.OnExit(e);
+        }
     }
 }
